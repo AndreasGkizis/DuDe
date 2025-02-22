0990f13aee29040cfb3ed2711dc9c3a72d11fe34
Andreas Gkizis <andreasgkizis@gmail.com>
2025-02-22 13:43:03 +0200

    memory still not working

--- internal/processing/memory.go
+++ internal/processing/memory.go
@@ -6,44 +6,61 @@ import (
 	"fmt"
 	"sync"
 	"time"
+
+	"go.uber.org/zap/zapcore"
 )
 
 var (
 	memory_buffer     []*models.FileHashBatch
-	runningSchedulers = make(chan struct{}, 1) // change number to adjust concurrent shedulers
+	runningSchedulers = make(chan struct{}, 1) // change number to adjust concurrent schedulers
 )
 
 func StartMemoryUpdateBackgroundProcess(path string, memoryChan <-chan models.FileHash) {
 	batchSize := uint(2) // for test reasons , should be larger
 	fmt.Print("StartMemoryUpdateBackgroundProcess")
 
-	ticker := time.NewTicker(5 * time.Second) // Write every 5 seconds
+	ticker := time.NewTicker(5 * time.Millisecond) // Write every 5 seconds
 	defer ticker.Stop()
 
 	var mutex sync.Mutex
 	go func() {
 		currentBatch := models.NewFileBatch(batchSize)
-		for newHash := range memoryChan {
-			mutex.Lock()
-			// if full, unload and create a new one
-			if len(currentBatch.Entries) >= int(currentBatch.BatchSize) {
-				memory_buffer = append(memory_buffer, currentBatch)
-				currentBatch = models.NewFileBatch(batchSize)
-				mutex.Unlock() // Unlock after appending and before next iteration
-				continue       // Important: Continue to avoid double unlock
+
+		for {
+			select {
+			case newHash := <-memoryChan:
+				mutex.Lock()
+				// if full, unload and create a new one
+				if len(currentBatch.Entries) >= int(currentBatch.BatchSize) {
+					memory_buffer = append(memory_buffer, currentBatch)
+					currentBatch = models.NewFileBatch(batchSize)
+					mutex.Unlock() // Unlock after appending and before next iteration
+					continue       // Important: Continue to avoid double unlock
+				}
+
+				currentBatch.Entries = append(currentBatch.Entries, newHash)
+				mutex.Unlock()
+			case t := <-ticker.C:
+				mutex.Lock()
+				logger.GetLogger().Log(zapcore.DebugLevel, t)
+				// only adds to buffer if there is something in this batch
+				if len(currentBatch.Entries) > 0 { // Write even if batch is not full
+					memory_buffer = append(memory_buffer, currentBatch)
+					currentBatch = models.NewFileBatch(batchSize) // Start a new batch
+				}
+				mutex.Unlock()
+				scheduleWriter(path)
+			default:
+				mutex.Lock()
+				// only adds to buffer if there is something in this batch
+				if len(currentBatch.Entries) > 0 { // Write even if batch is not full
+					memory_buffer = append(memory_buffer, currentBatch)
+					currentBatch = models.NewFileBatch(batchSize) // Start a new batch
+				}
+				mutex.Unlock()
+				go scheduleWriter(path)
 			}
-
-			currentBatch.Entries = append(currentBatch.Entries, newHash)
-			mutex.Unlock()
 		}
-		// mutex.Lock()
-		// // only adds to buffer if there is something in this batch
-		// if len(currentBatch.Entries) > 0 {
-		// 	memory_buffer = append(memory_buffer, currentBatch)
-		// }
-		// mutex.Unlock()
-
-		scheduleWriter(path)
 	}()
 }
 
@@ -59,7 +76,7 @@ func scheduleWriter(memoryPath string) {
 		logger.GetLogger().Debugf("semaphore released!")
 	}()
 
-	time.Sleep(3 * time.Second) // start writing in 3 second intervals
+	// time.Sleep(3 * time.Second) // start writing in 3 second intervals
 
 	mutex.Lock()
 	defer mutex.Unlock()
