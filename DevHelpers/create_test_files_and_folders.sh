376dcd36aff5c4524237e516075146e4b1ac7776
Andreas <andreasgkizis@gmail.com>
2025-03-01 11:21:10 +0200

    absolute path for testfiles, adds prefix

--- DevHelpers/create_test_files_and_folders.sh
+++ DevHelpers/create_test_files_and_folders.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# Create a base directory for the sample files
-BASE_DIR="../testfiles/WorkingCopies/Test"
-BASE_TARGET_DIR="../testfiles/WorkingCopies/Test_target"
+# Get the directory of this script
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-mkdir -p "$BASE_DIR"
-mkdir -p "$BASE_TARGET_DIR"
+# Create a base directory for the sample files
+BASE_DIR="../testfiles/source"
+BASE_TARGET_DIR="../testfiles/target"
+
+# Create a base directory for the sample files relative to the script's location
+BASE_DIR="$SCRIPT_DIR/../testfiles/source"
+BASE_TARGET_DIR="$SCRIPT_DIR/../testfiles/target"
 
 # Create subdirectories for different file types
 mkdir -p "$BASE_DIR/text_files"
@@ -18,26 +22,28 @@ mkdir -p "$BASE_TARGET_DIR/audio_files"
 
 create_sample_files(){
     local dir="$1"
+    local prefix="$2"
+
     # Create text files
-    echo "This is a sample text file." > "$dir/text_files/has_duplicate.txt"
-    cp "$dir/text_files/has_duplicate.txt" "$dir/text_files/has_duplicate1.txt" # Duplicate
-    echo "Another unique text file." > "$dir/text_files/has_no_duplicate.txt"
+    echo "This is a sample text file." > "$dir/text_files/"$prefix"-has_duplicate.txt"
+    cp "$dir/text_files/"$prefix"-has_duplicate.txt" "$dir/text_files/"$prefix"-has_duplicate1.txt" # Duplicate
+    echo "Another unique text file." > "$dir/text_files/"$prefix"-has_no_duplicate.txt"
 
     # Create image files with random bytes
-    openssl rand -out "$dir/image_files/has_duplicate.jpg" 1024
-    cp "$dir/image_files/has_duplicate.jpg" "$dir/image_files/has_duplicate1.jpg" # Duplicate
-    openssl rand -out "$dir/image_files/has_no_duplicate.png" 2048
+    openssl rand -out "$dir/image_files/"$prefix"-has_duplicate.jpg" 1024
+    cp "$dir/image_files/"$prefix"-has_duplicate.jpg" "$dir/image_files/"$prefix"-has_duplicate1.jpg" # Duplicate
+    openssl rand -out "$dir/image_files/"$prefix"-has_no_duplicate.png" 2048
 
     # Create audio files with random bytes
-    openssl rand -out "$dir/audio_files/has_duplicate.mp3" 512
-    cp "$dir/audio_files/has_duplicate.mp3" "$dir/audio_files/has_duplicate1.mp3" # Duplicate
-    openssl rand -out "$dir/audio_files/has_no_duplicate.wav" 1024
+    openssl rand -out "$dir/audio_files/"$prefix"-has_duplicate.mp3" 512
+    cp "$dir/audio_files/"$prefix"-has_duplicate.mp3" "$dir/audio_files/"$prefix"-has_duplicate1.mp3" # Duplicate
+    openssl rand -out "$dir/audio_files/"$prefix"-has_no_duplicate.wav" 1024
 
     echo "Sample files created in $dir"
 }
 
-create_sample_files "$BASE_DIR"
-create_sample_files "$BASE_TARGET_DIR"
+create_sample_files "$BASE_DIR" "source"
+create_sample_files "$BASE_TARGET_DIR" "target"
 
 copy_files_from_to(){
     local source="$1"
